4b1c965c37d38d3f6a74dc42a192d231d7886334
joshneal77 <joshneal.rent@gmail.com>
2026-06-30 22:38:06 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE='steadymen-v1';
-const FILES=['./','./index.html','./manifest.webmanifest','./assets/banner.jpg','./assets/crest.jpg','./downloads/Steady-Men-Summer-Study-Package.pdf'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
